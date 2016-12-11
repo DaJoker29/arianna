75b2ee5e6d8337fa34fa716b681153a4ba073484
Dewitt Buckingham <dj729@comcast.net>
2016-12-10 19:46:27 -0800

    Created a bot user

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-const WebClient = require('@slack/client').WebClient;
 const schedule = require('node-schedule');
+const WebClient = require('@slack/client').WebClient;
+const RtmClient = require('@slack/client').RtmClient;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
 const config = require('./config.json');
+
+// const reminders = require('./libs/reminders.js');
 
 /**
  * Arianna - Zero Daedalus Slackbot
@@ -11,36 +16,47 @@ const config = require('./config.json');
 console.log('Arianna Running...');
 
 const token = config.SLACK_TOKEN || '';
-const web = new WebClient(token);
+const rtm = new RtmClient(token);
 
-const botOpts = {
-  username: 'Arianna',
-  icon_emoji: ':arianna:',
-};
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (data) => {
+  console.log(`Logged in as ${data.self.name} of team ${data.team.name}, but not yet connected to a channel`);
+});
 
-const messages = [
-  'South America is waiting. Take a step closer today.',
-  'All you want exists just beyond your comfort zone.',
-  'Show those around you that you love them by helping to solve one of their problems.',
-  'You only have one body. Show it some respect today. Exercise, eat right and meditate.',
-  'It takes a lifetime to build a good reputation and a moment to destroy it. Do some good today.',
-  'Stress and anxiety are silent killers. Look after your mental health today.',
-  'Go deeper today. Try to solve a problem that you couldn\'t before.',
-];
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+  rtm.sendMessage('Back online...', config.SLACK_CHANNEL);
+});
 
-function reminder() {
-  const today = new Date();
-  const day = today.getDay();
+rtm.start();
+// const web = new WebClient(token);
 
-  web.chat.postMessage('D349GLKS5', messages[day], botOpts, (err, res) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(`Message Sent: ${JSON.stringify(res)}`);
-    }
-  });
-}
+// const botOpts = {
+//   username: 'Arianna',
+//   icon_emoji: ':arianna:',
+// };
 
-schedule.scheduleJob('0 */8 * * *', reminder);
-console.log('Testing...');
-reminder();
+// const messages = [
+//   'South America is waiting. Take a step closer today.',
+//   'All you want exists just beyond your comfort zone.',
+//   'Show those around you that you love them by helping to solve one of their problems.',
+//   'You only have one body. Show it some respect today. Exercise, eat right and meditate.',
+//   'It takes a lifetime to build a good reputation and a moment to destroy it.
+//    Do some good today.',
+//   'Stress and anxiety are silent killers. Look after your mental health today.',
+//   'Go deeper today. Try to solve a problem that you couldn\'t before.',
+// ];
+
+// function reminder() {
+//   const today = new Date();
+//   const day = today.getDay();
+
+//   web.chat.postMessage('D349GLKS5', messages[day], botOpts, (err, res) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(`Message Sent: ${JSON.stringify(res)}`);
+//     }
+//   });
+// }
+
+// schedule.scheduleJob('0 */8 * * *', reminders);
+// console.log('Testing...');
