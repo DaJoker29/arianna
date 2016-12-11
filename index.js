5f83cdb562f6c28b2a0a6076053f7ea5748cbcf8
Dewitt Buckingham <dj729@comcast.net>
2016-12-10 20:02:46 -0800

    Personal Motivators

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 const schedule = require('node-schedule');
-const WebClient = require('@slack/client').WebClient;
 const RtmClient = require('@slack/client').RtmClient;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 
@@ -24,39 +23,25 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (data) => {
 
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
   rtm.sendMessage('Back online...', config.SLACK_CHANNEL);
+  reminder();
 });
 
+schedule.scheduleJob('0 */7 * * *', reminder); 
+
 rtm.start();
-// const web = new WebClient(token);
 
-// const botOpts = {
-//   username: 'Arianna',
-//   icon_emoji: ':arianna:',
-// };
+function reminder() {
+  const today = new Date();
+  const day = today.getDay();
+  const messages = [
+    'South America is waiting. Take a step closer today.',
+    'All you want exists just beyond your comfort zone.',
+    'Show those around you that you love them by helping to solve one of their problems.',
+    'You only have one body. Show it some respect today. Exercise, eat right and meditate.',
+    'It takes a lifetime to build a good reputation and a moment to destroy it. Do some good today.',
+    'Stress and anxiety are silent killers. Look after your mental health today.',
+    'Go deeper today. Try to solve a problem that you couldn\'t before.',
+  ];
 
-// const messages = [
-//   'South America is waiting. Take a step closer today.',
-//   'All you want exists just beyond your comfort zone.',
-//   'Show those around you that you love them by helping to solve one of their problems.',
-//   'You only have one body. Show it some respect today. Exercise, eat right and meditate.',
-//   'It takes a lifetime to build a good reputation and a moment to destroy it.
-//    Do some good today.',
-//   'Stress and anxiety are silent killers. Look after your mental health today.',
-//   'Go deeper today. Try to solve a problem that you couldn\'t before.',
-// ];
-
-// function reminder() {
-//   const today = new Date();
-//   const day = today.getDay();
-
-//   web.chat.postMessage('D349GLKS5', messages[day], botOpts, (err, res) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log(`Message Sent: ${JSON.stringify(res)}`);
-//     }
-//   });
-// }
-
-// schedule.scheduleJob('0 */8 * * *', reminders);
-// console.log('Testing...');
+  rtm.sendMessage(messages[day], 'D3DAMT9UL'); // Send a personalized message to me daily
+}
