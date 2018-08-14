734faf79f7d73c8a31719fb93b6bbe7e8034e205
Dewitt Buckingham <dj729@comcast.net>
2018-08-13 20:53:48 -0700

    Updated to latest packages

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const schedule = require('node-schedule');
-const SQLite = require('sqlite3').verbose();
-const RtmClient = require('@slack/client').RtmClient;
-const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
-const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+const { RTMClient } = require('@slack/client');
 
 const config = require('./config.json');
 const reminders = require('./reminders.json');
@@ -22,29 +19,23 @@ if ('string' !== typeof config.SLACK_TOKEN) {
   process.exit(1);
 }
 
-if (!fs.existsSync(config.DB_PATH)) {
-  console.log('Joke database not found');
-  process.exit(1);
-}
-
-const rtm = new RtmClient(config.SLACK_TOKEN);
-const db = new SQLite.Database(config.DB_PATH);
+const rtm = new RTMClient(config.SLACK_TOKEN);
 
 let bot = '';
 
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (data) => {
+rtm.on('authenticated', (data) => {
   bot = data.self;
   console.log(`Logged in as ${data.self.name} of team ${data.team.name}, but not yet connected to a channel`);
 });
 
-rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+rtm.on('ready', () => {
   console.log('Connection opened...');
 });
 
-rtm.on(RTM_EVENTS.MESSAGE, msgHandler);
+rtm.on('message', msgHandler);
 
 // Schedule Repeated Tasks
-schedule.scheduleJob('0 */6 * * *', reminder);
+// schedule.scheduleJob('0 */6 * * *', reminder);
 schedule.scheduleJob('0 */6 * * *', quote);
 
 // Watch Slack
@@ -56,16 +47,11 @@ rtm.start();
 
 function msgHandler(message) {
   if (checkMessage(message)) {
-    // Check for Chuck Norris references
-    if (-1 < message.text.toLowerCase().indexOf('chuck norris')) {
-      db.get('SELECT id, joke FROM jokes ORDER BY used ASC, RANDOM() LIMIT 1', (err, record) => {
-        if (err) {
-          return console.error('DATABASE ERROR:', err);
-        }
-
-        rtm.sendMessage(record.joke, message.channel);
-        db.run('UPDATE jokes SET used = used + 1 WHERE id = ?', record.id);
-      });
+    console.log(`Incoming message: ${message.ts}`);
+    if ('quote' === message.text.toLowerCase()) {
+      quote();
+    } else if ('reminder' === message.text.toLowerCase()) {
+      reminder();
     }
   }
 }
@@ -95,7 +81,11 @@ function quote() {
       const rand = Math.floor(Math.random() * lines.length);
 
       const quote = lines[rand].match(/".+"/g)[0].slice(1, -1);
-      rtm.sendMessage(`> *_${quote}_*`, config.SLACK_CHANNEL);
+      rtm.sendMessage(`> *_${quote}_*`, config.SLACK_CHANNEL)
+        .then((res) => {
+          console.log(`Message sent: ${res.ts}`);
+        })
+        .catch(console.error);
     }
   });
 }
